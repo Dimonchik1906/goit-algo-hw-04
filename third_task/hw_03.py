08c4ad856fc7368786dc20b5f935a54580f5e3ab
Dimonchik1906 <85458459+Dimonchik1906@users.noreply.github.com>
2025-01-11 21:26:45 +0200

    12313

---
+++ third_task/hw_03.py
@@ -0,0 +1,44 @@
+from sys import argv
+from pathlib import Path
+from colorama import Fore, Back, Style
+
+def parth_folder(path, depth:int = 0):
+    groups = {True: [], False: []}
+    count = 0
+    path = Path(path)
+    
+    if path.exists() and path.is_dir():
+        
+        if depth == 0:
+            print(Back.BLACK, '📦', Fore.GREEN, path.absolute().name, sep='')
+    
+        for item in path.iterdir():
+            groups[item.is_dir()].append(item.name)
+            count += 1
+            
+        for is_dir, items in groups.items():
+            for item in sorted(items):
+                count -= 1
+            
+                print(Back.BLACK,
+                        Fore.WHITE,
+                        '┃ ' * depth,
+                        '┣ ' if count else '┗ ',
+                        '📂' if is_dir else '📜',
+                        Fore.BLACK if is_dir else Fore.BLUE,
+                        item,
+                        sep='',)
+                
+                if is_dir:
+                    parth_folder(f'{path}/{item}', depth + 1)
+                
+    else:
+        print(f'{Back.BLACK}{Fore.RED}{path} is absent or it is a file.')
+        
+    print(Style.RESET_ALL, end='')
+        
+def main():
+    parth_folder(argv[1] if len(argv) > 1 else '.')
+
+if __name__ == '__main__':
+    main()
